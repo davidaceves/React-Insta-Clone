751d3fc286111c534726d9d1453a0d9cbb984c2e
David <digital0david@gmail.com>
2019-03-13 22:02:30 -0400

    postcontainer, css

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 import dummyData from './dummy-data';
+import PostContainer from './components/PostContainer/PostContainer';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dummyData: dummyData
+    }
+  }
   render() {
     return (
       <div className="App">
-
+        <PostContainer data={this.state.dummyData} />
       </div>
     );
   }
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
+import './PostContainer.css';
+import PropTypes from "prop-types";
+
 function PostContainer (props) {
     return (
-
+        <div>
+            {props.data.map(entry => {
+               return <div className="postContainer">
+                        <header className="igHeader">
+                            <img src={entry.thumbnailUrl} alt="thumbnail" />
+                            {entry.username}
+                        </header>
+                        <img src={entry.imageUrl} alt="img"/>
+                     </div>
+            })}
+            
+        </div>
     )
 }
 
